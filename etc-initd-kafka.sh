f401c35ef79f349b66e2d3bd596b7ed4799ac813
Henryk Konsek <hekonsek@gmail.com>
2020-02-07 15:53:13 +0100

    Exported Kafka server Prometheus metrics.

--- etc-initd-kafka.sh
+++ etc-initd-kafka.sh
@@ -5,11 +5,12 @@
 # Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: kafka
-# Description:       kafka
+# Short-Description: Kafka server
+# Description:       Kafka server
 ### END INIT INFO
 
 start() {
+  export KAFKA_OPTS="${KAFKA_OPTS} -javaagent:/home/ubuntu/kafka_2.12-2.4.0/jmx_prometheus_javaagent-0.12.0.jar=9090:/home/ubuntu/kafka_2.12-2.4.0/kafka-0-8-2.yml"
   /home/ubuntu/kafka_2.12-2.4.0/bin/kafka-server-start.sh -daemon /home/ubuntu/kafka_2.12-2.4.0/config/server.properties
 }
 
